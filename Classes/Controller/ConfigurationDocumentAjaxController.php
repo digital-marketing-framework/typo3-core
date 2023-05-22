ff9b651e2ad66fbf3fd5f96bab02edf44db08580
Stephan Ude <ude@mediatis.de>
2023-05-22 09:02:09 +0200

    [TASK] configuration editor server-handling

--- Classes/Controller/ConfigurationDocumentAjaxController.php
+++ Classes/Controller/ConfigurationDocumentAjaxController.php
@@ -2,7 +2,10 @@
 
 namespace DigitalMarketingFramework\Typo3\Core\Controller;
 
+use DigitalMarketingFramework\Core\ConfigurationDocument\ConfigurationDocumentManagerInterface;
+use DigitalMarketingFramework\Core\Utility\ConfigurationUtility;
 use DigitalMarketingFramework\Typo3\Core\ConfigurationDocument\Event\ConfigurationDocumentMetaDataUpdateEvent;
+use DigitalMarketingFramework\Typo3\Core\Registry\Registry;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -10,26 +13,79 @@ use TYPO3\CMS\Core\EventDispatcher\EventDispatcher;
 
 class ConfigurationDocumentAjaxController
 {
-    protected ResponseFactoryInterface $responseFactory;
-    protected EventDispatcher $eventDispatcher;
+    protected ConfigurationDocumentManagerInterface $configurationDocumentManager;
 
     public function __construct(
-        ResponseFactoryInterface $responseFactory,
-        EventDispatcher $eventDispatcher,
+        protected ResponseFactoryInterface $responseFactory,
+        protected EventDispatcher $eventDispatcher,
+        Registry $registry,
     ) {
-        $this->responseFactory = $responseFactory;
-        $this->eventDispatcher = $eventDispatcher;
+        $this->configurationDocumentManager = $registry->getConfigurationDocumentManager();
     }
 
-    public function metaDataAction(ServerRequestInterface $request): ResponseInterface
+    protected function jsonResponse(array $result): ResponseInterface
     {
-        $event = new ConfigurationDocumentMetaDataUpdateEvent();
-        $this->eventDispatcher->dispatch($event);
-        $result = $event->toArray();
-
         $response = $this->responseFactory->createResponse()
             ->withHeader('Content-Type', 'application/json; charset=utf-8');
         $response->getBody()->write(json_encode($result, JSON_PRETTY_PRINT|JSON_THROW_ON_ERROR));
         return $response;
+    }
+
+    public function schemaAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $event = new ConfigurationDocumentMetaDataUpdateEvent();
+        $this->eventDispatcher->dispatch($event);
+        return $this->jsonResponse($event->getConfigurationSchema());
+    }
+
+    public function defaultsAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $event = new ConfigurationDocumentMetaDataUpdateEvent();
+        $this->eventDispatcher->dispatch($event);
+        return $this->jsonResponse($event->getDefaultConfiguration());
+    }
+
+    public function mergeAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $document = json_decode((string)$request->getBody(), true)['document'] ?? '';
+        $configurationStack = $this->configurationDocumentManager->getConfigurationStackFromDocument($document);
+        $configuration = ConfigurationUtility::mergeConfigurationStack($configurationStack);
+        return $this->jsonResponse($configuration);
+    }
+
+    protected function splitConfiguration(array $mergedConfiguration): array
+    {
+        $configurationStack = $this->configurationDocumentManager->getConfigurationStackFromConfiguration($mergedConfiguration);
+        array_pop($configurationStack);
+        $parentConfiguration = ConfigurationUtility::mergeConfigurationStack($configurationStack);
+        return ConfigurationUtility::splitConfiguration($parentConfiguration, $mergedConfiguration);
+    }
+
+    public function splitAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $mergedConfiguration = json_decode((string)$request->getBody(), true);
+        $splitConfiguration = $this->splitConfiguration($mergedConfiguration);
+        $splitDocument = $this->configurationDocumentManager->getParser()->produceDocument($splitConfiguration);
+        return $this->jsonResponse(['document' => $splitDocument]);
+    }
+
+    public function updateIncludesAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $data = json_decode((string)$request->getBody(), true);
+        $referenceData = $data['referenceData'];
+        $newData = $data['newData'];
+
+        $oldIncludes = $referenceData['includes'];
+        $newIncludes = $newData['includes'];
+
+        $mergedData = $newData;
+        $mergedData['includes'] = $oldIncludes;
+        $splitData = $this->splitConfiguration($mergedData);
+
+        $splitData['includes'] = $newIncludes;
+        $configurationStack = $this->configurationDocumentManager->getConfigurationStackFromConfiguration($splitData);
+        $configuration = ConfigurationUtility::mergeConfigurationStack($configurationStack);
+
+        return $this->jsonResponse($configuration);
     }
 }
--- Configuration/Backend/AjaxRoutes.php
+++ Configuration/Backend/AjaxRoutes.php
@@ -4,7 +4,23 @@ use DigitalMarketingFramework\Typo3\Core\Controller\ConfigurationDocumentAjaxCon
 
 return [
     'digitalmarketingframework_configuration_schema' => [
-        'path' => '/digital-marketing-framework-backend/schema',
-        'target' => ConfigurationDocumentAjaxController::class . '::metaDataAction',
+        'path' => '/digital-marketing-framework-backend/configuration-document/schema',
+        'target' => ConfigurationDocumentAjaxController::class . '::schemaAction',
+    ],
+    'digitalmarketingframework_configuration_defaults' => [
+        'path' => '/digital-marketing-framework-backend/configuration-document/defaults',
+        'target' => ConfigurationDocumentAjaxController::class . '::defaultsAction',
+    ],
+    'digitalmarketingframework_configuration_merge' => [
+        'path' => '/digital-marketing-framework-backend/configuration-document/merge',
+        'target' => ConfigurationDocumentAjaxController::class . '::mergeAction',
+    ],
+    'digitalmarketingframework_configuration_split' => [
+        'path' => '/digital-marketing-framework-backend/configuration-document/split',
+        'target' => ConfigurationDocumentAjaxController::class . '::splitAction',
+    ],
+    'digitalmarketingframework_configuration_update_includes' => [
+        'path' => '/digital-marketing-framework-backend/configuration-document/update-includes',
+        'target' => ConfigurationDocumentAjaxController::class . '::updateIncludesAction',
     ],
 ];

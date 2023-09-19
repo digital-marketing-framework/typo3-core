006174ba146d2ab8502b71af3c2f0c6d47a96ce4
Stephan Ude <ude@mediatis.de>
2023-09-19 12:42:14 +0200

    [TASK] update global configuration

--- Classes/GlobalConfiguration/GlobalConfiguration.php
+++ Classes/GlobalConfiguration/GlobalConfiguration.php
@@ -2,17 +2,16 @@
 
 namespace DigitalMarketingFramework\Typo3\Core\GlobalConfiguration;
 
-use DigitalMarketingFramework\Core\Model\Configuration\Configuration;
+use DigitalMarketingFramework\Core\GlobalConfiguration\DefaultGlobalConfiguration;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationExtensionNotConfiguredException;
 use TYPO3\CMS\Core\Configuration\Exception\ExtensionConfigurationPathDoesNotExistException;
 use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
 
-class GlobalConfiguration extends Configuration
+class GlobalConfiguration extends DefaultGlobalConfiguration
 {
     public function __construct(
         protected ExtensionConfiguration $extensionConfiguration,
     ) {
-        parent::__construct([]);
     }
 
     public function get(string $key = '', mixed $default = null): mixed

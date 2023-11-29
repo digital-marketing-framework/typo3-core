42d7092fd3ac4a0bc69d3352fa6a3436a941b23e
Stephan Ude <ude@mediatis.de>
2023-11-29 20:56:02 +0100

    remove auto include of typoscript

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,23 +2,12 @@
 
 use DigitalMarketingFramework\Typo3\Core\Backend\DataHandler\JsonDataHandler;
 use DigitalMarketingFramework\Typo3\Core\Controller\FrontendController;
-use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use DigitalMarketingFramework\Typo3\Core\Form\Element\JsonFieldElement;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
 
 call_user_func(static function () {
-    ExtensionManagementUtility::addTypoScript(
-        'dmf_core',
-        'constants',
-        "@import 'EXT:dmf_core/Configuration/TypoScript/constants.typoscript'"
-    );
-    ExtensionManagementUtility::addTypoScript(
-        'dmf_core',
-        'setup',
-        "@import 'EXT:dmf_core/Configuration/TypoScript/setup.typoscript'"
-    );
 
     // Json Field - pretty printed in backend UI, compressed in DB
     $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = JsonDataHandler::class;

624f3c6c9980612613c04b2197fd4b06db92140c
Stephan Ude <ude@mediatis.de>
2023-11-29 20:55:41 +0100

    standardised backend field type json

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,7 +1,9 @@
 <?php
 
+use DigitalMarketingFramework\Typo3\Core\Backend\DataHandler\JsonDataHandler;
 use DigitalMarketingFramework\Typo3\Core\Controller\FrontendController;
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use DigitalMarketingFramework\Typo3\Core\Form\Element\JsonFieldElement;
 use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 defined('TYPO3') || die();
@@ -17,6 +19,14 @@ call_user_func(static function () {
         'setup',
         "@import 'EXT:dmf_core/Configuration/TypoScript/setup.typoscript'"
     );
+
+    // Json Field - pretty printed in backend UI, compressed in DB
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass'][] = JsonDataHandler::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeRegistry'][1673431342] = [
+        'nodeName' => JsonFieldElement::RENDER_TYPE,
+        'priority' => 40,
+        'class' => JsonFieldElement::class,
+    ];
 
     ExtensionUtility::configurePlugin(
         'DmfCore',

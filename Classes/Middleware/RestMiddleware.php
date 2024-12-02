9778a593f514af72d2b63583cc182b1e29462202
Stephan Ude <ude@mediatis.de>
2024-12-02 15:40:24 +0100

    wip

--- Classes/Middleware/RestMiddleware.php
+++ Classes/Middleware/RestMiddleware.php
@@ -16,15 +16,15 @@ use Psr\Http\Server\RequestHandlerInterface;
 
 class RestMiddleware implements MiddlewareInterface
 {
-    protected EntryRouteResolverInterface $routeResolver;
+    // protected EntryRouteResolverInterface $routeResolver;
 
     public function __construct(
         protected StreamFactoryInterface $streamFactory,
         protected ResponseFactoryInterface $responseFactory,
         protected EventDispatcherInterface $eventDispatcher,
-        protected RegistryCollection $registryCollection,
+        // protected RegistryCollection $registryCollection,
     ) {
-        $this->routeResolver = $this->registryCollection->getApiEntryRouteResolver();
+        // $this->routeResolver = $this->registryCollection->getApiEntryRouteResolver();
     }
 
     protected function buildResponse(ApiResponseInterface $apiResponse): ResponseInterface
@@ -64,28 +64,18 @@ class RestMiddleware implements MiddlewareInterface
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-//        print('<pre>');
-//        print_r([
-//            'requestPath' => $request->getUri()->getPath(),
-//            'requestParams' => $request->getQueryParams(),
-//            'apiEnabled' => $this->routeResolver->enabled(),
-//            'apiBasePath' => $this->routeResolver->getBasePath(),
-//        ]);
-//        exit;
-        $requestPath = ltrim($request->getUri()->getPath(), '/');
-        if ($this->routeResolver->enabled() && str_starts_with($requestPath, $this->routeResolver->getBasePath())) {
-        // if (array_key_exists('dmfResource', $request->getQueryParams()) && $this->routeResolver->enabled()) {
-        // if (array_key_exists('dmfResource', $request->getQueryParams())) {
+        // $requestPath = ltrim($request->getUri()->getPath(), '/');
+        // if ($this->routeResolver->enabled() && str_starts_with($requestPath, $this->routeResolver->getBasePath())) {
             return $this->responseFactory->createResponse(200, '')
                 ->withHeader('Content-Type', 'application/json; charset=utf-8')
                 ->withHeader('Cache-Control', 'no-store, must-revalidate')
                 ->withBody($this->streamFactory->createStream('true'));
 
 
-//            $apiResponse = $this->processRequest($request);
-//
-//            return $this->buildResponse($apiResponse);
-        }
+        //    $apiResponse = $this->processRequest($request);
+
+        //    return $this->buildResponse($apiResponse);
+        // }
 
         return $handler->handle($request);
     }

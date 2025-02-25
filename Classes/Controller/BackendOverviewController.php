32d5913e92c6c589577944162e62464107f2c5be
s-ude <ude@mediatis.de>
2025-02-25 23:03:25 +0100

    [FEATURE] Alerts (#19)

--- Classes/Controller/BackendOverviewController.php
+++ Classes/Controller/BackendOverviewController.php
@@ -2,12 +2,48 @@
 
 namespace DigitalMarketingFramework\Typo3\Core\Controller;
 
+use DigitalMarketingFramework\Core\Model\Alert\AlertInterface;
+use DigitalMarketingFramework\Typo3\Core\Registry\RegistryCollection;
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Backend\Template\ModuleTemplateFactory;
+use TYPO3\CMS\Core\Imaging\IconFactory;
 
 class BackendOverviewController extends AbstractBackendController
 {
+    public function __construct(
+        ModuleTemplateFactory $moduleTemplateFactory,
+        IconFactory $iconFactory,
+        protected RegistryCollection $registryCollection,
+    ) {
+        parent::__construct($moduleTemplateFactory, $iconFactory);
+    }
+
+    /**
+     * @return array<array{title:?string,content:string,source:string,state:int}>
+     */
+    protected function getAlerts(): array
+    {
+        $messages = [];
+        foreach ($this->registryCollection->getAlertManager()->getAllAlerts() as $message) {
+            $messages[] = [
+                'title' => $message->getTitle(),
+                'content' => $message->getContent(),
+                'source' => $message->getSource(),
+                'state' => match ($message->getType()) {
+                    AlertInterface::TYPE_INFO => 0,
+                    AlertInterface::TYPE_WARNING => 1,
+                    AlertInterface::TYPE_ERROR => 2,
+                    default => 0,
+                },
+            ];
+        }
+
+        return $messages;
+    }
+
     public function showAction(): ResponseInterface
     {
+        $this->view->assign('alerts', $this->getAlerts());
         $this->view->assign('sections', $this->getSections());
 
         return $this->backendHtmlResponse();

94e0bdeca79a7bfdd71a6ab4f2595febb0bc93f4
Stephan Ude <ude@mediatis.de>
2025-05-21 19:47:55 +0200

    MFR-136 pipeline test

--- rector.php
+++ rector.php
@@ -8,5 +8,5 @@ use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void
 {
-    Typo3RectorSetup::setup($rectorConfig, __DIR__, 12, PhpVersion::PHP_82);
+    Typo3RectorSetup::setup($rectorConfig, __DIR__, 12);
 };

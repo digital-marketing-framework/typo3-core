559bd0da61b8f57f8e0b3089c4acdfa9b0897c60
Stephan Ude <ude@mediatis.de>
2025-05-25 22:02:43 +0200

    MFR-136 cleanup

--- rector.php
+++ rector.php
@@ -4,9 +4,8 @@ declare(strict_types=1);
 
 use Mediatis\Typo3CodingStandards\Php\Typo3RectorSetup;
 use Rector\Config\RectorConfig;
-use Rector\ValueObject\PhpVersion;
 
 return static function (RectorConfig $rectorConfig): void
 {
-    Typo3RectorSetup::setup($rectorConfig, __DIR__, 12);
+    Typo3RectorSetup::setup($rectorConfig, __DIR__);
 };

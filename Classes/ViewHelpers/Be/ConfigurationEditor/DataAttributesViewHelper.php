ccf5b1b80aeb403c61afb8876b00654bc9abd9f7
s-ude <ude@mediatis.de>
2025-07-09 18:51:03 +0200

    [TASK] Enable Configuration Editor Context (#27)

--- Classes/ViewHelpers/Be/ConfigurationEditor/DataAttributesViewHelper.php
+++ Classes/ViewHelpers/Be/ConfigurationEditor/DataAttributesViewHelper.php
@@ -3,7 +3,8 @@
 namespace DigitalMarketingFramework\Typo3\Core\ViewHelpers\Be\ConfigurationEditor;
 
 use DigitalMarketingFramework\Core\ConfigurationEditor\MetaData;
-use DigitalMarketingFramework\Typo3\Core\Utility\ConfigurationEditorRenderUtility;
+use DigitalMarketingFramework\Typo3\Core\Registry\RegistryCollection;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\ViewHelpers\Be\AbstractBackendViewHelper;
 
 class DataAttributesViewHelper extends AbstractBackendViewHelper
@@ -18,6 +19,8 @@ class DataAttributesViewHelper extends AbstractBackendViewHelper
         $this->registerArgument('documentType', 'string', 'Document type for ajax URLs', false, MetaData::DEFAULT_DOCUMENT_TYPE);
         $this->registerArgument('includes', 'bool', 'Configuration support includes', false, true);
         $this->registerArgument('routeParameters', 'array', 'Additional AJAX route parameters', false, []);
+        $this->registerArgument('contextIdentifier', 'string', 'Context identifier', false, '');
+        $this->registerArgument('uid', 'string', 'Unique editor identifier', false, '');
     }
 
     /**
@@ -25,14 +28,16 @@ class DataAttributesViewHelper extends AbstractBackendViewHelper
      */
     public function render(): array
     {
-        return ConfigurationEditorRenderUtility::getTextAreaDataAttributes(
+        return GeneralUtility::makeInstance(RegistryCollection::class)->getRegistry()->getBackendRenderingService()->getTextAreaDataAttributes(
             ready: $this->arguments['ready'],
             mode: $this->arguments['mode'],
             readonly: $this->arguments['readonly'],
             globalDocument: $this->arguments['globalDocument'],
             documentType: $this->arguments['documentType'],
             includes: $this->arguments['includes'],
-            parameters: $this->arguments['routeParameters']
+            parameters: $this->arguments['routeParameters'],
+            contextIdentifier: $this->arguments['contextIdentifier'],
+            uid: $this->arguments['uid']
         );
     }
 }
--- Classes/ViewHelpers/Be/ConfigurationEditor/TextAreaViewHelper.php
+++ Classes/ViewHelpers/Be/ConfigurationEditor/TextAreaViewHelper.php
@@ -3,7 +3,8 @@
 namespace DigitalMarketingFramework\Typo3\Core\ViewHelpers\Be\ConfigurationEditor;
 
 use DigitalMarketingFramework\Core\ConfigurationEditor\MetaData;
-use DigitalMarketingFramework\Typo3\Core\Utility\ConfigurationEditorRenderUtility;
+use DigitalMarketingFramework\Typo3\Core\Registry\RegistryCollection;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Fluid\ViewHelpers\Be\AbstractBackendViewHelper;
 
 class TextAreaViewHelper extends AbstractBackendViewHelper
@@ -20,20 +21,24 @@ class TextAreaViewHelper extends AbstractBackendViewHelper
         $this->registerArgument('documentType', 'string', 'Document type for ajax URLs', false, MetaData::DEFAULT_DOCUMENT_TYPE);
         $this->registerArgument('includes', 'bool', 'Configuration support includes', false, true);
         $this->registerArgument('routeParameters', 'array', 'Additional AJAX route parameters', false, []);
+        $this->registerArgument('contextIdentifier', 'string', 'Context identifier', false, '');
+        $this->registerArgument('uid', 'string', 'Unique editor identifier', false, '');
 
         $this->registerArgument('additionalAttributes', 'array', 'Additional attributes', false, []);
     }
 
     public function render(): string
     {
-        $attributes = ConfigurationEditorRenderUtility::getTextAreaDataAttributes(
+        $attributes = GeneralUtility::makeInstance(RegistryCollection::class)->getRegistry()->getBackendRenderingService()->getTextAreaDataAttributes(
             ready: $this->arguments['ready'],
             mode: $this->arguments['mode'],
             readonly: $this->arguments['readonly'],
             globalDocument: $this->arguments['globalDocument'],
             documentType: $this->arguments['documentType'],
             includes: $this->arguments['includes'],
-            parameters: $this->arguments['routeParameters']
+            parameters: $this->arguments['routeParameters'],
+            contextIdentifier: $this->arguments['contextIdentifier'],
+            uid: $this->arguments['uid']
         );
 
         $attributeMarkupList = [];

835a51680c709e1309312438c68a166f5aa8fc17
Stephan Ude <ude@mediatis.de>
2025-07-23 19:33:59 +0200

    typo3 12 compatibility

--- Classes/Crypto/HashService.php
+++ Classes/Crypto/HashService.php
@@ -3,22 +3,38 @@
 namespace DigitalMarketingFramework\Typo3\Core\Crypto;
 
 use DigitalMarketingFramework\Core\Crypto\HashServiceInterface;
-use TYPO3\CMS\Core\Crypto\HashService as Typo3HashService;
+use TYPO3\CMS\Core\Crypto\HashService as Typo3V13HashService;
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Security\Cryptography\HashService as Typo3V12HashService;
 
 class HashService implements HashServiceInterface
 {
-    public function __construct(
-        protected Typo3HashService $hashService,
-    ) {
-    }
-
     public function generateHash(string $subject, string $additionalSecret): string
     {
-        return $this->hashService->hmac($subject, $additionalSecret);
+        $version = new Typo3Version();
+        if ($version->getMajorVersion() <= 12) {
+            $hashService = GeneralUtility::makeInstance(Typo3V12HashService::class);
+
+            return $hashService->generateHmac($subject);
+        }
+
+        $hashService = GeneralUtility::makeInstance(Typo3V13HashService::class);
+
+        return $hashService->hmac($subject, $additionalSecret);
     }
 
     public function validateHash(string $subject, string $additionalSecret, string $hash): bool
     {
-        return $this->hashService->validateHmac($subject, $additionalSecret, $hash);
+        $version = new Typo3Version();
+        if ($version->getMajorVersion() <= 12) {
+            $hashService = GeneralUtility::makeInstance(Typo3V12HashService::class);
+
+            return $hashService->validateHmac($subject, $hash);
+        }
+
+        $hashService = GeneralUtility::makeInstance(Typo3V13HashService::class);
+
+        return $hashService->validateHmac($subject, $additionalSecret, $hash);
     }
 }
